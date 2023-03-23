6f835668bdbacf96b1d91c362ea0a3142c06e72c
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-03-23 10:43:07 +0100

    feat: no more localhost

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,9 +1,9 @@
 <div>
-  <a href="http://localhost:5173/wireframes"><h1>Storytelling/ Wireframes</h1></a>
-  <a href="http://localhost:5173/styleguide"><h1>Styleguide</h1></a>
-  <a href="http://localhost:5173/copyright"><h1>Copyright</h1></a>
-  <a href="http://localhost:5173/licenses"><h1>Licenses</h1></a>
-  <a href="http://localhost:5173/image-types"><h1>Image Filetypes</h1></a>
-  <a href="http://localhost:5173/video-types"><h1>Video Filetypes</h1></a>
-  <a href="http://localhost:5173/audio-types"><h1>Audio Filetypes</h1></a>
+  <a href="wireframes"><h1>Storytelling/ Wireframes</h1></a>
+  <a href="styleguide"><h1>Styleguide</h1></a>
+  <a href="copyright"><h1>Copyright</h1></a>
+  <a href="licenses"><h1>Licenses</h1></a>
+  <a href="image-types"><h1>Image Filetypes</h1></a>
+  <a href="video-types"><h1>Video Filetypes</h1></a>
+  <a href="audio-types"><h1>Audio Filetypes</h1></a>
 </div>
