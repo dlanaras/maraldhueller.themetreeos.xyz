1fd220722a6cb90392a0f1f087e41fd545467cb9
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-03-23 09:59:12 +0100

    muh

---
+++ src/components/footer.svelte
@@ -0,0 +1,5 @@
+<div style="flex">
+    <h4>Modul 152</h4>
+    <h4>Dimitrios Lanaras</h4>
+    <h4>🄯 {new Date().getFullYear()}</h4>
+</div>
---
+++ src/components/header.svelte
@@ -0,0 +1,9 @@
+<div>
+  <a href="http://localhost:5173/wireframes"><h1>Storytelling/ Wireframes</h1></a>
+  <a href="http://localhost:5173/styleguide"><h1>Styleguide</h1></a>
+  <a href="http://localhost:5173/copyright"><h1>Copyright</h1></a>
+  <a href="http://localhost:5173/licenses"><h1>Licenses</h1></a>
+  <a href="http://localhost:5173/image-types"><h1>Image Filetypes</h1></a>
+  <a href="http://localhost:5173/video-types"><h1>Video Filetypes</h1></a>
+  <a href="http://localhost:5173/audio-types"><h1>Audio Filetypes</h1></a>
+</div>
