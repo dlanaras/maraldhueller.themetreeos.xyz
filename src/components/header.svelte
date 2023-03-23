301e21af8cbb34a70bff8a5ff79e1839e682b25e
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-03-23 10:51:54 +0100

    test

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,9 +1,9 @@
 <div>
-  <a href="wireframes"><h1>Storytelling/ Wireframes</h1></a>
-  <a href="styleguide"><h1>Styleguide</h1></a>
-  <a href="copyright"><h1>Copyright</h1></a>
-  <a href="licenses"><h1>Licenses</h1></a>
-  <a href="image-types"><h1>Image Filetypes</h1></a>
-  <a href="video-types"><h1>Video Filetypes</h1></a>
-  <a href="audio-types"><h1>Audio Filetypes</h1></a>
+  <a href="/wireframes"><h1>Storytelling/ Wireframes</h1></a>
+  <a href="/styleguide"><h1>Styleguide</h1></a>
+  <a href="/copyright"><h1>Copyright</h1></a>
+  <a href="/licenses"><h1>Licenses</h1></a>
+  <a href="/image-types"><h1>Image Filetypes</h1></a>
+  <a href="/video-types"><h1>Video Filetypes</h1></a>
+  <a href="/audio-types"><h1>Audio Filetypes</h1></a>
 </div>
