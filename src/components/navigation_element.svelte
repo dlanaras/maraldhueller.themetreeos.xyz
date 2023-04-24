2c245d883cc2f2a7ae9c7f5fd05552af25663c29
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-04-24 15:25:42 +0200

    test

--- src/components/navigation_element.svelte
+++ src/components/navigation_element.svelte
@@ -2,16 +2,14 @@
   import { page } from "$app/stores";
 
   export let path = "/";
-  export let name = "";
+  export let name = "m152";
 </script>
 
 
 <a href={path} class={$page.url.pathname === path ? "active" : ""}>
-  {#if (this.path !== "/")}
+
     <h3>{name}</h3>
-  {:else}
-    <!--img src="%sveltekit.assets%/logo.png" alt="maraldhueller logo"-->
-  {/if}
+
 </a>
 
 
