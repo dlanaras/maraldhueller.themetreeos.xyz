7f75ba65a71bef0abe301221ea6db6faf5c53903
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-05-02 22:35:14 +0200

    feat: added logo

--- src/components/navigation_element.svelte
+++ src/components/navigation_element.svelte
@@ -7,7 +7,11 @@
 
 
 <a href={path} class={$page.url.pathname === path ? "active" : ""}>
+  {#if (path !== "/")}
   <h3>{name}</h3>
+  {:else}
+  <img src="hm-logo.png" alt="HM logo" width="48px" height="48px">
+  {/if}
 </a>
 
 
@@ -33,5 +37,10 @@
   .active {
     background-color: #003f88;
     cursor: pointer;
+  }
+
+  img {
+    margin-top: 16px;
+    margin-bottom: 16px;
   }
 </style>
