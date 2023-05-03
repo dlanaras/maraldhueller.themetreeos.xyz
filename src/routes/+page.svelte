9fefe3527c5904dff8b618dc0aad66abf01ed466
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-05-03 22:18:09 +0200

    muh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,9 +18,23 @@
     <div>
         <h2>Hauptseite</h2>
     </div>
-    <div>
-        <!--img src="%sveltekit.assets%/logo.png" alt="maraldhueller logo"-->
+    <div class="img">
+        <img src="hm-logo.png" alt="maraldhueller logo" width="512px">
+        <h3>MaraldHueller</h3>
+        <h3>.</h3>
+        <h3>TheMeTreeOs</h3>
+        <h3>.</h3>
+        <h3>xyz</h3>
     </div>
 </div>
 <Footer/>
 {/if}
+
+<style>
+    .img {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
