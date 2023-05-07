3b907c1050d1aae90d6ddb03305eb2c209ef1acf
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-05-07 16:22:48 +0200

    feat: dsgvo done

--- src/routes/cookies/+page.svelte
+++ src/routes/cookies/+page.svelte
@@ -15,7 +15,14 @@
 <Header/>
 <div id="content">
     <div>
-        <h2>DSGVO/ Cookies</h2>
+        <h2>DSGVO - Datenschutz-Grundverordnung und Cookies</h2>
+        <h3>Was ist die DSGVO?</h3>
+        <p>Ganz simpel regelt die DSGVO, was genau mit Endbenutzerdaten, die Firmen sammeln, alles gemacht werden kann.
+           Alle diese Regeln müssen zumindest in der EU eingehalten werden, und bei Nichteinhaltung können gesetzliche Strafen folgen.
+           Eines der vielen Dinge, die man nicht tun darf, ist die Verwendung der Daten einer Person ohne deren Zustimmung.
+        </p>
+        <h3>Wann muss ein Cookie-Banner auf einer Website verwendet werden?</h3>
+        <p>Sobald man Cookies verwendet, die für die Funktionalität der Website nicht nötig sind (oder wenn das eine Bewertungskriterie ist für das Modul 152 ;) ).</p>
     </div>
 </div>
 <Footer/>
