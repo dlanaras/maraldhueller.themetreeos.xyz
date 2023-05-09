0c9502f4c6c89e5724e1acffb2ee4be08d5922f5
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-05-09 15:16:24 +0200

    feat: reflection done + whole project

--- src/routes/reflection/+page.svelte
+++ src/routes/reflection/+page.svelte
@@ -1,21 +1,32 @@
-<script>
+<script lang="ts">
     import Header from '../../components/header.svelte'
     import Footer from '../../components/footer.svelte';
     import Loading from '../../components/loading.svelte';
     import { onMount } from 'svelte';
+    import saveAs from 'file-saver';
 
-    let ready = false;
+    let url = ``;
 
-    onMount(() => ready = true)
+    onMount(() => url = window.location.origin);
+
+    function promptDownload(file: string): void {
+        saveAs(`${url}/${file}`, file); 
+    }
 </script>
 
-{#if !ready}
+{#if url === ""}
     <Loading/>
 {:else}
 <Header/>
 <div id="content">
     <div>
         <h2>Reflexion</h2>
+        <h3>Was habe ich neugelernt?</h3>
+        Seit dem ich meine Applikationsentwickler Lehrstelle begonnen habe, habe ich schon mehrmals Websites gebaut und habe deswegen bei diesen Projekt Vieles schon gekonnt.
+        Trotzdem könnte ich vieles über SVGs und CSS-Animationen neulernen, da ich damit nicht so oft gearbeitet hatte. Ausserdem habe ich das Framework Svelte zum ersten Mal verwendet
+        und könnte jetzt zumindest die Basics davon kennen lernen.
+        <h3>Testprotokoll</h3>
+        <button on:click={() => promptDownload("Testprotokoll_m152_dimitrios.docx")}>Testprotokoll herunterladen</button>
     </div>
 </div>
 <Footer/>
